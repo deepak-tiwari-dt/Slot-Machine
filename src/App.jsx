b1653b31844b002e0c6f67002fd97ab4864965db
Deepak Tiwari <dt@Deepaks-MacBook-Air.local>
2023-08-03 00:14:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Slots from "./Slots";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Slots val1="🍎" val2="🍎" val3="🍎" />
+      <Slots val1="🍒" val2="🍉" val3="🍒" />
+    </div>
+  );
+}
+
+export default App;
